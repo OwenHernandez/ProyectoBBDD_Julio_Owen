13c24605848fb4d52d95d8b4b0177dad988314cb
JulioGlezGlez <juliomanuelgonzalezgonzalez@gmail.com>
2023-05-22 20:19:11 +0100

    Se crea creacion.sql

---
+++ sql/creacion.sql
@@ -0,0 +1,9 @@
+create DATABASE GestionDocente;
+use GestionDocente;
+
+create table persona(
+    id int not null auto_increment,
+    nombre varchar(100),
+    PRIMARY KEY(id)
+    id_Aula int references
+);
