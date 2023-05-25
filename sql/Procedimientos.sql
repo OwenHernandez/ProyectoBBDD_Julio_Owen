f602ed7901b2d03cddbbc53443429a8038ea90a6
OwenHernandez <113590599+OwenHernandez@users.noreply.github.com>
2023-05-25 14:31:56 -0400

    Update Procedimientos.sql

--- sql/Procedimientos.sql
+++ sql/Procedimientos.sql
@@ -8,7 +8,7 @@ begin
   declare nombreRnd varchar(100);
   set idRnd = (select count(*)+1 from persona);
   set contador=1;
-  while (contador<=numeroIns) do
+  while (contador<=num_inserts) do
     set nombreRnd = concat("Persona", idRnd);
     insert into persona values(idRnd, nombreRnd);
     contador++;
@@ -23,15 +23,21 @@ drop procedure if exists Insert_gestiona;
 delimiter//
 create procedure Insert_gestiona(in num_inserts int)
 begin
-  declare idRnd int;
-  declare nombreRnd varchar(100);
-  set idRnd = (select count(*)+1 from persona);
+  declare id_DocenteRnd int;
+  declare id_CursoRnd int;
+  set counter=(select count(*)+1 from docente);
+  set id_DocenteRnd=(select id from docente where id=counter);
   set contador=1;
-  while (contador<=numeroIns) do
-    set nombreRnd = concat("Persona", idRnd);
-    insert into persona values(idRnd, nombreRnd);
+  while (contador<=num_inserts) do
+    if id_DocenteRnd!=null then
+      set id_DocenteRnd=(select id from docente where id=counter);
+    end if;
+    if id_Curso !=null then
+      set id_CursoRnd=(select id from curso where id=counter);
+    end if;
+    insert into alumno values(id_CursoRnd, id_DocenteRnd);
     contador++;
-    idRnd++;
+    counter++;
   end while;
 end
 //
