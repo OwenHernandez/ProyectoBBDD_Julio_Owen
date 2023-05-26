d5126a349ecda8f5c461c803a69068b889976032
Owen-Daniel-Hernández-Hanrahan <owentoto423@gamil.com>
2023-05-26 15:48:51 +0100

    hecho la vista del calendario

--- sql/vistas.sql
+++ sql/vistas.sql
@@ -1,32 +1,45 @@
 --vista docente, persona, gestiona y curso
 
 create view lista_Docente as
-select 
-  d.id as DID, 
-  p.nombre as nombre, 
-  d.trabajo as trabajo, 
+select
+  d.id as DID,
+  p.nombre as nombre,
+  d.trabajo as trabajo,
   group_concat(concat(c.numero, "º", c.letra, _utf8mb4' ', c.tipo) separator ", ") as curso
-from 
+from
   persona as p join docente as d
-    ON p.id = d.id_Persona 
+    ON p.id = d.id_Persona
   JOIN gestiona as g
     ON d.id = g.id_Docente
-  JOIN curso as c 
+  JOIN curso as c
     ON g.id_Curso = c.id
 GROUP BY d.id;
 
 --vista alumno, persona, gestiona y curso
 
 create view lista_Alumno as
-select 
-  a.id as AID, 
+select
+  a.id as AID,
   p.nombre as nombre,
   a.faltas as faltas,
-  a.actitud as actitud, 
+  a.actitud as actitud,
   group_concat(concat(c.numero, "º", c.letra, _utf8mb4' ', c.tipo) separator ", ") as curso
-from 
+from
   persona as p join alumno as a
-    ON p.id = a.id_Persona 
-  JOIN curso as c 
+    ON p.id = a.id_Persona
+  JOIN curso as c
     ON a.id_Curso = c.id
 GROUP BY a.id;
+
+--vista calendario_faltas y alumno
+
+create view calendario as
+select
+  c.id as CID,
+  group_concat(concat(p.nombre, _utf8mb4' ', c.fecha_Falta) separator ", ") as fecha_alumno
+from
+  persona as p join alumno as a
+    ON p.id = a.id_Persona
+  JOIN calendario_faltas as c
+    ON a.id = c.id_Alumno
+GROUP BY c.id;
