eff76880f13ff35b7a585027e7efb927768356c4
Owen-Daniel-Hernández-Hanrahan <owentoto423@gamil.com>
2023-05-26 15:41:57 +0100

    hecha vista para alumno

--- sql/vistas.sql
+++ sql/vistas.sql
@@ -3,8 +3,8 @@
 create view lista_Docente as
 select 
   d.id as DID, 
-  d.trabajo as trabajo, 
   p.nombre as nombre, 
+  d.trabajo as trabajo, 
   group_concat(concat(c.numero, "º", c.letra, _utf8mb4' ', c.tipo) separator ", ") as curso
 from 
   persona as p join docente as d
@@ -14,3 +14,19 @@ from
   JOIN curso as c 
     ON g.id_Curso = c.id
 GROUP BY d.id;
+
+--vista alumno, persona, gestiona y curso
+
+create view lista_Alumno as
+select 
+  a.id as AID, 
+  p.nombre as nombre,
+  a.faltas as faltas,
+  a.actitud as actitud, 
+  group_concat(concat(c.numero, "º", c.letra, _utf8mb4' ', c.tipo) separator ", ") as curso
+from 
+  persona as p join alumno as a
+    ON p.id = a.id_Persona 
+  JOIN curso as c 
+    ON a.id_Curso = c.id
+GROUP BY a.id;
