2d44577abff9d82d675fb34f1fb89886e2e1af97
OwenHernandez <113590599+OwenHernandez@users.noreply.github.com>
2023-05-27 22:19:53 +0100

    Update indices.sql

--- sql/indices.sql
+++ sql/indices.sql
@@ -1,5 +1,3 @@
-CREATE INDEX nombreIndice ON nombreTabla(campo1 [,campo2...]);
-
 --Indice Docente
 
 create fulltext index idx_trabajo on docente(trabajo);
@@ -14,19 +12,20 @@ create index idx_fechas on calendario_faltas(fecha_Falta);
 
 --Indice persona
 
-
+create index idx_nombre_persona on (nombre);
 
 --Indice alumno
 
-
+create index idx_actitud_alumno on (faltas, actitud);
 
 --Indice grupo
 
-
+create index idx_integrantes_grupo on (integrantes);
 
 --Indice actividad
 
-
+create index idx_tipo_actividad on (tipo);
 
 --Indice nota
 
+create index idx_valor_nota on (numero);
